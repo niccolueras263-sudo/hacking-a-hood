f2b7b8f2d922a2b47dfe1499c541ad485d5dd880
Nicco Lueras <nicco@Niccos-MacBook-Pro.local>
2026-06-25 12:44:14 -0700

    updating lab-3

---
+++ lab-3.py
@@ -0,0 +1,13 @@
+#nicco lueras | Lab 3 | blue
+#python3 lab3_nicco.py
+name = nicco
+print.len(nicco)
++
+username(8) = "x"
+print("hi, my name is nicco")
+.append()
+.pop()
+"username, followers, verified".
+follower = 50
+bio.
+.get()

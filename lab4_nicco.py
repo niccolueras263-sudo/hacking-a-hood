f5c888ddcc7d0d0dd9238df4b0a75a1630acf05f
niccolueras263-sudo <niccolueras263@gmail.com>
2026-06-30 15:10:20 -0700

    Create lab4_nicco.py
    week 4 lab4

---
+++ lab4_nicco.py
@@ -0,0 +1,15 @@
+#Jordan Lee | Lab 4 | Intro To Python
+#python5 lab5_nicco.py
+ages = (17, 11, 25, 13, 9)
+17 = access granted
+11 = too young
+25 = access granted
+13 = access granted
+9 = too young
+keep_chcking = "yes"
+while == keep_checking = "yes":
+(user) "enter age"
+(keep checking) age is "13" - older
+check 
+while true:
+user: enter age group (or) type "stop

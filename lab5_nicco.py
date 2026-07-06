c9b350df27b94171608203f920c3b342ef909ce4
niccolueras263-sudo <niccolueras263@gmail.com>
2026-07-06 12:11:54 -0700

    Create lab5_nicco.py

---
+++ lab5_nicco.py
@@ -0,0 +1,12 @@
+Lab 5 | Nicco
+total = likes - shares 
+total = likes + shares
+make shoutout ("jo", "cool")
+print(make_shoutout("Jo", "cool"))
+make_shoutout("Alex")
+make_shoutout("Alex", "chill")
+count_hype("ten", 5)
+count_hype(10, 5)
+mood = "hyped"
+print(f"{mod}")
+print(f"{mood}")

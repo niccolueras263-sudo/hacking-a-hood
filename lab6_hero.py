c6d1700be1137001fee105d73f9ddbe56053e239
niccolueras263-sudo <niccolueras263@gmail.com>
2026-07-14 14:57:20 -0700

    Create lab6_hero.py

---
+++ lab6_hero.py
@@ -0,0 +1,19 @@
+import random
+from ability import ability
+from armor import ability
+
+class hero: 
+def__init__(self, name, starting_health=100): #starting_health=100
+self.name = Spiderman
+self.starting.health - starting_health
+self.current_health = starting_health
+self.abilities = [super.speed, spider_sense]
+self.armors + [regenoration]
+
+def battle(self, opponent): # chooses a random winner between 2 heroes
+my_list = [self.spiderman, opponent. doc ock]
+print(random.choice(my_list))
+
+def add_ability(self, ability): # adds an ability to heroes list of abilities
+self.abilities.append(ability)
+ability_1.attack()
